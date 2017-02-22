534ac9ce889f4dea9a103510eae5c88c21b04467
ITSPaul <powell.paul@itsligo.ie>
2017-02-22 10:53:37 +0000

    initial commit

---
+++ Queue1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Queue1
+{
+    class Program
+    {
+        static Queue<int> choices = new Queue<int>();
+        static void Main(string[] args)
+        {
+            int choice = 0;
+            choices.Enqueue(4);
+            choices.Enqueue(2);
+            choices.Enqueue(1);
+            char ans = '0';
+            
+            while(ans != 'q')
+            {
+                Console.WriteLine("Press 'q' to quit 't' to toggle");
+                ans = Console.ReadKey().KeyChar;
+                if(ans == 't')
+                {
+                    choice = choices.Dequeue();
+                    choices.Enqueue(choice);
+                    Console.WriteLine("Current choice {0}",choice);
+                }
+
+            }
+        }
+    }
+}
